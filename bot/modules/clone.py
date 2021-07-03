9e4ec7afbcf29eac374388d34e0ee72a27d9ebf2
kick2809 <83411241+kick2809@users.noreply.github.com>
2021-07-03 15:43:53 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -23,10 +23,10 @@ def cloneNode(update,context):
             else:
                 uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
             if uname is not None:
-                cc = f'\n\nReq. By: {uname}'
+                cc = f'\n\n♂/♀/⚣/⚢👉:- {uname}'
             sendMarkup(result + cc, context.bot, update, button)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
+        sendMessage("Bruh Provide G-Drive Shareable Link to Clone.",context.bot,update)
 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 dispatcher.add_handler(clone_handler)
